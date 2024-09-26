28268e3f0b7daa87ca0550a4ae3fcc1c9175130b
Gavin <gavc8943@gmail.com>
2024-09-26 10:49:50 +0100

    Assignments and Challenges
    Basic C# praftice

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+        Console.WriteLine(" he said \"go away\" ");
+        Console.ReadLine();
+
+        }
+    }
+
