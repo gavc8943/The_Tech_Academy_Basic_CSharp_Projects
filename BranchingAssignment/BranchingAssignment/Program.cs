bb91f3f65ab936a68cbcacafd6582ed3e74e000c
Gavin <gavc8943@gmail.com>
2024-09-29 12:37:18 +0100

    added comments
    comments added

--- BranchingAssignment/BranchingAssignment/Program.cs
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -12,11 +12,11 @@ namespace BranchingAssignment
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
             Console.WriteLine("Please enter your package weight.");
-            int packageWeight = Convert.ToInt32(Console.ReadLine());
+            int packageWeight = Convert.ToInt32(Console.ReadLine()); // receive input and Convert to Int
 
-            if (packageWeight < 50)     // 
+            if (packageWeight < 50)     //  if else statement , using comparison operator
             {
-                Console.WriteLine("Thank you");
+                Console.WriteLine("Thank you"); 
             }
             else if (packageWeight >= 50)
             {
@@ -24,7 +24,7 @@ namespace BranchingAssignment
                 //return;  // Would end the program
             }
 
-            Console.WriteLine("Please enter package width");
+            Console.WriteLine("Please enter package width");  // taking input for width height and length and converting to int
             int packageWidth = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Please enter package height");
@@ -33,9 +33,9 @@ namespace BranchingAssignment
             Console.WriteLine("Please enter package length");
             int packagelength = Convert.ToInt32(Console.ReadLine());
 
-            decimal total = Convert.ToDecimal(packageWidth + packageheight + packagelength);
+            decimal total = Convert.ToDecimal(packageWidth + packageheight + packagelength); // created total variable 
 
-            if (total >= 50)
+            if (total >= 50)  // another if else statement
             {
                 Console.WriteLine("Package too big to be shipped by Package Express");
                 //return; // would exit program
@@ -45,7 +45,7 @@ namespace BranchingAssignment
                 Console.WriteLine("Thank you");
             }
 
-            decimal quote = Convert.ToDecimal((packageWidth * packagelength * packageheight) * packageWeight / 100);
+            decimal quote = Convert.ToDecimal((packageWidth * packagelength * packageheight) * packageWeight / 100); // quote variable created and converted to decimal for monetary value
 
             Console.WriteLine("Your estimated total for shiiping this package is $" + quote);
             Console.WriteLine("Thank you");
