d7c2eff17b5d5c9325f7dd8bef783069834dcbd0
Gavin <gavc8943@gmail.com>
2024-09-29 12:34:24 +0100

    Branching Assignment
    Using if else and ternery

---
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below");
+            Console.WriteLine("Please enter your package weight.");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (packageWeight < 50)     // 
+            {
+                Console.WriteLine("Thank you");
+            }
+            else if (packageWeight >= 50)
+            {
+                Console.WriteLine("Package too big to be shipped by Package Express. Have a nice day");
+                //return;  // Would end the program
+            }
+
+            Console.WriteLine("Please enter package width");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter package height");
+            int packageheight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter package length");
+            int packagelength = Convert.ToInt32(Console.ReadLine());
+
+            decimal total = Convert.ToDecimal(packageWidth + packageheight + packagelength);
+
+            if (total >= 50)
+            {
+                Console.WriteLine("Package too big to be shipped by Package Express");
+                //return; // would exit program
+            }
+            else
+            {
+                Console.WriteLine("Thank you");
+            }
+
+            decimal quote = Convert.ToDecimal((packageWidth * packagelength * packageheight) * packageWeight / 100);
+
+            Console.WriteLine("Your estimated total for shiiping this package is $" + quote);
+            Console.WriteLine("Thank you");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- ConditionStatementsChallenge/ConditionStatementsChallenge/Program.cs
+++ ConditionStatementsChallenge/ConditionStatementsChallenge/Program.cs
@@ -10,21 +10,70 @@ namespace ConditionStatementsChallenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Which is your favorite meal? (Chicken , Beef or Fish?)");
-            string faveFood = Console.ReadLine().ToLower();
+            //Console.WriteLine("Which is your favorite meal? (Chicken , Beef or Fish?)");
+            //string faveFood = Console.ReadLine().ToLower();
 
-            if (faveFood == "chicken")
-            {
-                Console.WriteLine("Enyoy your chicken dinner");
-            }
-            else if (faveFood == "beef")
-            {
-                Console.WriteLine("Enjoy your beef dinner");
-            }
-            else if (faveFood == "fish") 
-            {
-                Console.WriteLine("Enjoy your fish supper");
-            }
+            //if (faveFood == "chicken")
+            //{
+            //    Console.WriteLine("Enyoy your chicken dinner");
+            //}
+            //else if (faveFood == "beef")
+            //{
+            //    Console.WriteLine("Enjoy your beef dinner");
+            //}
+            //else if (faveFood == "fish") 
+            //{
+            //    Console.WriteLine("Enjoy your fish supper");
+            //}
+            //Console.ReadLine();
+
+            // not operator
+
+            //Console.WriteLine("Please enter your name");
+            //string yourName = Console.ReadLine();
+
+            //if (yourName != "Gavin")
+            //{
+            //    Console.WriteLine("Hello nice to meet you");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hey thats my name too");
+            //}
+            //Console.ReadLine();
+
+            //Console.WriteLine("Please enter your age");
+            //int yourAge = Convert.ToInt32(Console.ReadLine());
+
+            //if (yourAge >= 18)
+            //{
+            //    Console.WriteLine("You may drink alcohol");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry you cant drink alcohol yet");
+            //}
+
+
+            //Console.WriteLine("How many pints do you drink on a night out?");
+            //int pintsDrank = Convert.ToInt32(Console.ReadLine());
+
+            //if (pintsDrank <= 3)
+            //{
+            //    Console.WriteLine("Thats a sensible amount");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You might have a hangover the next day");
+            //}
+
+            //Console.ReadLine();
+
+            int num1 = 22;
+            int num2 = 25;
+
+            string result = num1 < num2 ? "num1 is less than num2" : "num1 is not less than num2";
+            Console.WriteLine(result);
             Console.ReadLine();
         }
     }
