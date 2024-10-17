ee106634dbdafad5a81dc083e07acc5018e45200
Gavin <gavc8943@gmail.com>
2024-10-17 11:36:51 +0100

    Main method assignment
    create a class, instantiate it, use methods

---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the Math class
+            Math math1 = new Math();
+            // Call the first method with an int parameter
+            Console.WriteLine(math1.myMath(8));
+            // Call the second method with a decimal parameter
+            Console.WriteLine(math1.myMath(2.4m));
+            // Call the third method with a string parameter
+            Console.WriteLine(math1.myMath("8"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+      public class Math
+    {
+        // First Integer method
+        public int myMath(int num)
+        {
+            return num * 4; 
+        }
+        // Second Method that takes a decimal
+        public int myMath(decimal num)
+        {
+            // Convert decimal to int 
+            int myNum = Convert.ToInt32(num);
+            return myNum * 25;
+        }
+        // Third Method that takes a string
+
+        public int myMath(string num)
+        {
+            // Convert the string to an Int
+            int myNum = Convert.ToInt32(num);
+            return myNum / 2;
+        }
+    }
+}
