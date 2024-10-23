3d6b0edeea85caf305d4106ab84e5cf2cd8b43d6
Gavin <gavc8943@gmail.com>
2024-10-23 12:40:15 +0100

    Abstract Class Assignment
    create an abstract class, inheritance

---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate Employee Object
+
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName(); // Call the same name method on the object
+            Console.ReadLine();
+        }
+    }
+}
