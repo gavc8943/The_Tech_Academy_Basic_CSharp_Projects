595185a8020dfe9cd873c2cf6fc670f8688245b2
Gavin <gavc8943@gmail.com>
2024-10-24 14:55:02 +0100

    Polymorphism
    polymorphism example

---
+++ Polymorphism/Polymorphism/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Generic animal sound");
+        }
+
+        public void MakeAnimalSound(Animal animal)
+        {
+            animal.MakeSound();
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {           // 1 method => different implementations
+                // 1 Action Different behaviours based on object type
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+
+
+            animal.MakeAnimalSound(animal);
+            animal.MakeAnimalSound(dog);
+            animal.MakeAnimalSound(cat);
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
