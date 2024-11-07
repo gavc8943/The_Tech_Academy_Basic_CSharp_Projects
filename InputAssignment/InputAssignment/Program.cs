af2c6fa02100fd9999b5eda5ab35c4143ee0e266
Gavin <gavc8943@gmail.com>
2024-11-07 16:07:00 +0000

    input assignment

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number");
+            int yourNum = Convert.ToInt32(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\User\log\log.html", Convert.ToString(yourNum));
+            int fileContent = Convert.ToInt32(File.ReadAllText(@"C:\Users\User\log\log.html"));
+            Console.WriteLine("Your number is " + fileContent);
+            Console.ReadLine();
+        }
+    }
+}
