c1fa6b49fe2ccffeaf47e5d542f4cfbb2dc52659
Gavin <gavc8943@gmail.com>
2024-11-10 19:26:02 +0000

    ConstructorAssignment

---
+++ Constructor_Assignment/Constructor_Assignment/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; }
+
+        // Constructor that takes two parameters
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        // Constructor that takes 3 parameters Name, Age, and Occupation
+        // This constructor calls the first (constructor chaining)
+
+        public Person(string name, int age, string occupation) :this(name,age)
+        {
+            Occupation = occupation;
+        }
+
+        // Override the Tostring() method to display persons details in a readable format
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Occupation: {Occupation}"; // Return a formatted string with persons info
+        }
+    }
+}
---
+++ Constructor_Assignment/Constructor_Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Assignment
+{
+    class Program
+    {
+        const string Greeting = "Welcome!"; // const variable
+        static void Main(string[] args)
+        {
+            var name = "Michael"; // Var variable created
+
+            Console.WriteLine(Greeting);
+            Console.WriteLine("Hello " + name );
+
+            // Create a new object creating a class constructor
+            var person = new Person("Peter", 32); // calls the first constructor
+            var otherPerson = new Person("Jason", 42, "Barber"); // second constructor is called
+
+            Console.WriteLine(person.ToString());
+            Console.WriteLine(otherPerson.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
