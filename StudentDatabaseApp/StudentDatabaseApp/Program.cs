2e8b9b0f7c6bf227f10876b1bc8cc5790105f0d3
Gavin <gavc8943@gmail.com>
2024-12-03 11:22:31 +0000

    Updated student database app

---
+++ StudentDatabaseApp/StudentDatabaseApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using StudentDatabaseApp;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new StudentDbContext())
+        {
+            context.Database.EnsureCreated();
+
+            var student = new Student
+            {
+                FirstName = "John",
+                LastName = "Smith",
+                DateOfBirth = new DateTime(2000, 1, 1)
+            };
+
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            Console.WriteLine("Student added to the database.");
+        }
+    }
+}
+
---
+++ StudentDatabaseApp/StudentDatabaseApp/StudentDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;  // Needed to use EF Core classes
+
+namespace StudentDatabaseApp
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=StudentDb;Trusted_Connection=True;");
+        }
+    }
+
+}
